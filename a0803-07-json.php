85b1fd0c5fb3fb3b8bac74a9637a268b60f21585
Una Chang <una.c303@outlook.com>
2021-08-10 00:35:15 +0800

    json encode、decode、JSON_UNESCAPED_UNICODE

--- a0803-07-json.php
+++ a0803-07-json.php
@@ -6,30 +6,34 @@ for($i=1; $i<10; $i++) {
 }
 array_push($br, 100, 101);
 
+echo json_encode($br); //encode：轉換成json字串
 
-
-foreach($br as $v){
-    echo "$v <br>";  }
-// $v 為自己定義的變數，用他拿到$br內部的元素值
-
-echo '-------------------<br>';
+echo '<br>-------------------<br>';
 
 $ar = [
     'name' => 'David',
     'age' => 23,
     'gender' => 'male',
-    'hello'   //原則上不要混用索引式、關聯式
+    'address' => '台灣/高雄',
 ];
-
-foreach($ar as $k => $v){
-    echo "$k => $v <br>";
-}
-
-echo '-------------<br>';
-
-echo count($ar);
+echo json_encode($ar); 
+// 對應的是json的abject
+// \u代表unicode；
+// \u53f0代表一個中文字，53f0此四位數代表16進位數的編碼(unicode字碼)
+echo '<br>-------------------<br>';
 
 
+echo json_encode($ar, JSON_UNESCAPED_UNICODE); 
+// JSON_UNESCAPED_UNICODE： 中文字json不要跳脫成unicode
+echo '<br>-------------------<br>';
+
+echo json_encode($ar, JSON_UNESCAPED_SLASHES); 
+// JSON_UNESCAPED_SLASHES：slashes不要跳脫成unicode
+echo '<br>-------------------<br>';
+
+echo json_encode($ar, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES); 
+// 同時設定不跳拖中文字＆ slashes，在中間加'+' 或是'|' (位元運算值的or)
+echo '<br>-------------------<br>';
 
 
 
