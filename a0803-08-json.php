0ef6172b50c33c8e322196b4d64cb9e29c1036f7
Una Chang <una.c303@outlook.com>
2021-08-10 00:42:10 +0800

    chrome外掛，外掛，json viewer

---
+++ a0803-08-json.php
@@ -0,0 +1,17 @@
+
+<?php
+
+
+$ar = [
+    'name' => 'David',
+    'age' => 23,
+    'gender' => 'male',
+    'address' => '台灣/高雄',
+];
+echo json_encode($ar); 
+
+// 在傳送過程"有"跳脫成unicode，但可透過chrome擴充工具json viewer，看到原始的字串(方便除錯)
+
+//須整份檔案都是json才可啟動chrome外掛
+
+?>
