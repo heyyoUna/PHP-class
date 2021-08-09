8d843a54fc4deee0d6a8833b25b3ae375de4878c
Una Chang <una.c303@outlook.com>
2021-08-09 23:43:43 +0800

    php陣列下的foreach迴圈、as

--- a0803-06-foreach.php
+++ a0803-06-foreach.php
@@ -8,24 +8,38 @@ array_push($br, 100, 101);
 
 foreach($br as $v){
     echo "$v <br>";  }
-// $v 為自己定義的變數，用他拿到$br內部的元素值
-
+// foreach 迴圈的敘述
+// $v 為自定義變數，用他拿到$br內部的值
+ 
 echo '-------------------<br>';
 
-$ar = [
+$ar = array(
+    123,
     'name' => 'David',
-    'age' => 23,
+    'age' => 23, 
     'gender' => 'male',
     'hello'   //原則上不要混用索引式、關聯式
-];
+);
+//array() = []
 
 foreach($ar as $k => $v){
-    echo "$k => $v <br>";
-}
+    echo "$v <br>";}
+// foreach 也可使用在關聯式陣列
 
 echo '-------------<br>';
 
-echo count($ar);
+
+foreach($ar as $key => $value){
+    echo "$key => $value <br>";}
+// $key & $value 都是自定義變數
+//沒有key值的孩子，將依順序，用從0開始的數字補上
+
+echo '-------------<br>';
+
+echo count($ar); 
+//count 找出陣列的長度；此處混用索引式、關聯式，在foreach迴圈下也會將關聯式陣列納入計算(反之，for不會)
+//盡量不要混用索引式、關聯式!!
+//php陣列，建議使用foreach減少for
 
 
 
