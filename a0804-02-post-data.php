8442a2f1e4ef6932cdda799ce1ee056d344322f8
Una Chang <una.c303@outlook.com>
2021-08-10 14:43:29 +0800

    轉為 用法

---
+++ a0804-02-post-data.php
@@ -0,0 +1,14 @@
+
+<?php
+
+
+$a = isset($_POST['a']) ? intval($_POST['a']) : 0;
+
+
+$b = isset($_POST['b']) ? intval($_POST['b']) : 0;
+
+
+echo json_encode([
+    'postData' => $_POST,
+    'result' => $a + $b,
+]);
